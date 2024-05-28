95b9a5f880df9e99f3fc7dc0a819cbb7e4b9196c
dandanii22 <dandanii0816@gmail.com>
2024-05-28 20:02:49 +0900

    테스트 했던거 삭제

--- src/components/ImgView.jsx
+++ src/components/ImgView.jsx
@@ -4,7 +4,6 @@ import ImgList from "./ImgList";
 const ImgView = ({ data, test, onView, onShow }) => {
   return (
     <div>
-      11111
       <BigImg test={test} />
       <ImgList data={data} onView={onView} onShow={onShow} />
     </div>
